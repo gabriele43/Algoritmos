7e1f3bdea5f9a6059c1f93895b9bde7b04fc5b75
gabriele43 <162490335+gabriele43@users.noreply.github.com>
2024-03-08 17:22:13 -0300

    Add files via upload

---
+++ aula03/aula03.js
@@ -0,0 +1,11 @@
+//este é uma cometário
+console.log('ola mundo')
+const nome = 'gabriele'
+const idade = 26
+console.log('meu nome é ',nome,' e tenho',idade,' anos ')
+const altura = 1.45
+const temperatura = 45
+let souUmBoolean = true
+souUmBoolean = false
+
+const meuNome ''
---
+++ aula03/exercicios.js
@@ -0,0 +1,23 @@
+const nome = 'gabriele'
+const sobrenome = 'aparecida'
+const idade = 26
+const souEstudante = true
+console.log ('meu nome é' ,nome, 'meu sobrenome é' , sobrenome, 'minha idade é' ,idade, 'anos' ,souEstudante)
+const serie = 'game of thrones'
+const temp = 8
+
+//este comndo  impreme o tipo de valor 
+//pode ser strig, number, boolean
+console.log(typeof serie,typeof temporada)
+const minhaSerie = friends
+const asTemps = 10
+console.log('a serie' ,minhaSerie, typeof minhSerie, 'ela tem' ,asTemps,  typeof asTemps 'temporadas')  
+const idadeNumero = 25
+const idadetextos = idadeNumero.tostring()
+console.log(typeof idadenumero)
+console.log(typeofidadeTexto)
+const idadetexto2 = '23'
+const idadeNumero2 = Number(idadeTexto2)
+console.log(typeofidadeTexto2)
+console.log(typeofidadeNumero2)
+
